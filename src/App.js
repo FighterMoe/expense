300028730de4a3683b28d7c5fc037c375c1b3916
Zin Moe <zin.moe@gmail.com>
2023-10-10 20:27:38 +0630

    Project reinilization

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import Expenses from "./components/Expenses/Expenses"
+import React from "react"
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "New Car",
-      amount: 240.25,
-      date: new Date(2023, 3, 27),
-    },
-    {
-      id: "e2",
-      title: "12 Carret Diamon",
-      amount: 25.13,
-      date: new Date(2023, 8, 17),
-    },
-    {
-      id: "e3",
-      title: "New Appartment",
-      amount: 360.23,
-      date: new Date(2023, 9, 15),
-    },
-  ];
-
   return (
     <div>
-      <h2>Get Started.</h2>
-      <Expenses expenses={expenses} />
+      <h2>Wellcome React Dev.</h2>
     </div>
   );
 }
